2c6caef40f4deee8ff3f148bd76be25bbafa394c
ChangYongUm <cyum@curiosis.com>
2024-06-03 08:44:33 +0900

    2024.06.03

--- drivers/core/uclass.c
+++ drivers/core/uclass.c
@@ -538,7 +538,7 @@ int uclass_get_device_by_ofnode(enum uclass_id id, ofnode node,
 
 	if(ret) 
 	{
-		printf("%s  - result for %s: (ret=%d)\n", __func__, 
+		printf("%s  - result for %s: %s (ret=%d)\n", __func__, 
 	    	ofnode_get_name(node), dev ? dev->name : , ret); //test
 	}
 
