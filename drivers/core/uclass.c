98307e2b34f6aaa47c393ef7c17f27c5f92fb59d
ChangYongUm <cyum@curiosis.com>
2024-06-03 09:46:53 +0900

    2024.06.03

--- drivers/core/uclass.c
+++ drivers/core/uclass.c
@@ -398,7 +398,7 @@ int uclass_find_device_by_ofnode(enum uclass_id id, ofnode node,
 
 		if(id==UCLASS_DISPLAY || id==UCLASS_POWER_DOMAIN)
 		{
-			printf(" %s %s %s - checking %ld %ld\n",  uc->name, node.np->full_name, dev->name,  dev->node_.of_offset, node.of_offset); //test
+			printf(" %s %s - checking %ld %ld\n",  node.np->full_name, dev->name,  dev->node_.of_offset, node.of_offset); //test
 		}
 
 		if (ofnode_equal(dev_ofnode(dev), node)) {
