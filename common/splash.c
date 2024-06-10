2671f714b17c976fe511074152c9025bbf541d74
ChangYongUm <cyum@curiosis.com>
2024-06-10 10:45:41 +0900

    2024.06.10

--- common/splash.c
+++ common/splash.c
@@ -175,14 +175,13 @@ int splash_display(void)
 	if (x || y)
 		goto end;
 
-
-
-while(1);//test
-
 #if defined(CONFIG_DM_VIDEO) && !defined(CONFIG_HIDE_LOGO_VERSION)
 	splash_display_banner();
 #endif
 end:
+
+while(1);//test
+
 	return ret;
 }
 #endif
