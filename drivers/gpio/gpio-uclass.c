ec24434d68621ecee5554c267426d14630dfc65a
ChangYongUm <cyum@curiosis.com>
2024-06-11 13:51:33 +0900

    2024.06.11

--- drivers/gpio/gpio-uclass.c
+++ drivers/gpio/gpio-uclass.c
@@ -345,18 +345,10 @@ int gpio_hog_remove_all(void)
 		struct gpio_hog_data *plat = dev_get_plat(dev);
 		struct gpio_hog_priv *priv = dev_get_priv(dev);
 
-		ret = gpio_dev_request_index(dev->parent, dev->name, "gpio-hog",
-						plat->val[0], plat->gpiod_flags,
-						plat->val[1], &priv->gpiod);
-		if (ret < 0) 
-		{
-			printf("%s: node %s could not get gpio.\n", __func__, dev->name);
-			return ret;
-		}
-
 		if (plat->gpiod_flags == GPIOD_IS_OUT) 
 		{
-			ret = dm_gpio_set_value(&priv->gpiod, !plat->value);
+			int value - dm_gpio_get_value(&priv->gpiod);
+			ret = dm_gpio_set_value(&priv->gpiod, !value);
 			if (ret < 0) 
 			{
 				printf("%s: node %s could not set gpio.\n", __func__, dev->name);
